abda0e0f668af0d3797971609e63f843051ab5ee
skye220 <leyna.sassi@gmail.com>
2024-02-03 19:45:48 +0100

    first function for skye 220

--- reste_division.py
+++ reste_division.py
@@ -1,3 +1,4 @@
+# Function for calculating the remainder of a division
 def reste_division_entiere(dividende: int, diviseur: int):
     quotient = dividende // diviseur
     reste = dividende - (diviseur * quotient)
