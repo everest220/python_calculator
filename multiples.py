ed0c69619c5853d854bf10eedc5ce3b358459b72
skye220 <leyna.sassi@gmail.com>
2024-03-10 12:44:29 +0100

    Add function is_prime_number

--- multiples.py
+++ multiples.py
@@ -12,4 +12,10 @@ def multiple_number(number_to_test: int, multiple_to_use: int):
         return False
 
 
-
+def is_prime_number(number_to_test: int):
+    for multiple_to_use in range(2, number_to_test - 1):
+        result = multiple_number(number_to_test, multiple_to_use)
+        if result == True:
+            return False
+    # La boucle for est terminée. Tous les nombres ont été testés et on n'est jamais sorti de la fonction.
+    return True
